c547f97b13cf1ef6a34b304e3e677753aad539cf
Michele Spina <michelespina96@gmail.com>
2023-04-06 16:49:43 +0200

    automatize data_owner execution

--- architecture/data_owner.py
+++ architecture/data_owner.py
@@ -4,13 +4,14 @@ from hashlib import sha512
 import json
 from decouple import config
 import sqlite3
+import argparse
 
 process_instance_id = config('PROCESS_INSTANCE_ID')
 
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
-server_sni_hostname = 'example.com'
+server_sni_hostname = 'Sapienza'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 SERVER = "172.17.0.2"
 ADDR = (SERVER, PORT)
@@ -38,12 +39,13 @@ manufacturer_address = config('ADDRESS_MANUFACTURER')
 def sign_number():
     x.execute("SELECT * FROM handshake_number WHERE process_instance=?", (process_instance_id,))
     result = x.fetchall()
+    print(process_instance_id)
+    print(result)
     number_to_sign = result[0][2]
 
     x.execute("SELECT * FROM rsa_private_key WHERE reader_address=?", (sender,))
     result = x.fetchall()
     private_key = result[0]
-
     private_key_n = int(private_key[1])
     private_key_d = int(private_key[2])
 
@@ -69,7 +71,7 @@ def send(msg):
     conn.send(message)
     receive = conn.recv(6000).decode(FORMAT)
     if len(receive) != 0:
-
+        print(receive)
         if receive[:15] == 'Number to sign:':
             x.execute("INSERT OR IGNORE INTO handshake_number VALUES (?,?,?)",
                       (process_instance_id, sender, receive[16:]))
@@ -87,12 +89,12 @@ message_to_send = g.read()
 
 # policy_string = '1604423002081035210 and (MANUFACTURER or (SUPPLIER and ELECTRONICS))'
 
-entries = [['ID', 'SortAs', 'GlossTerm'], ['Acronym', 'Abbrev'], ['Specs', 'Dates']]
+entries = [['ID', 'SortAs', 'GlossTerm'], ['Acronym', 'Abbrev'], ['Specs', 'Dates']] 
 entries_string = '###'.join(str(x) for x in entries)
 
-policy = ['16216090141887661468 and (MANUFACTURER or SUPPLIER)',
-          '16216090141887661468 and (MANUFACTURER or (SUPPLIER and ELECTRONICS))',
-          '16216090141887661468 and (MANUFACTURER or (SUPPLIER and MECHANICS))']
+policy = ['1358911044885481786 and (MANUFACTURER or SUPPLIER)',
+          '1358911044885481786 and (MANUFACTURER or (SUPPLIER and ELECTRONICS))',
+          '1358911044885481786 and (MANUFACTURER or (SUPPLIER and MECHANICS))']
 policy_string = '###'.join(policy)
 
 # data = json.load(f)
@@ -105,11 +107,16 @@ policy_string = '###'.join(policy)
 
 sender = manufacturer_address
 
-# send("Start handshake||" + sender)
+parser = argparse.ArgumentParser()
+parser.add_argument('-hs' ,'--hanshake', action='store_true')
+parser.add_argument('-c','--cipher', action='store_true')
 
-signature_sending = sign_number()
+args = parser.parse_args()
+if args.hanshake:
+    send("Start handshake||" + sender)
 
-send("Cipher this message||" + message_to_send + '||' + entries_string + '||' + policy_string + '||' +
-     sender + '||' + str(signature_sending))
+if args.cipher:
+    signature_sending = sign_number()
+    send("Cipher this message||" + message_to_send + '||' + entries_string + '||' + policy_string + '||' + sender + '||' + str(signature_sending))
 
 send(DISCONNECT_MESSAGE)
---
+++ architecture/data_owner.sh
@@ -0,0 +1,2 @@
+python3 data_owner.py -hs
+python3 data_owner.py -c
